fafbf4501a14a63d8c3d19f6f978b693ed24c572
Pream Pinbut <momozahara@hotmail.com>
2021-11-01 15:34:13 +0700

    Update bgm

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let day = new Date().getDay();
-console.log(day)
+console.log(`Day of the week: ${day}`);
 
-let targetName, tagetVolume = 0.2;
+let targetName, targetVolume = 0.08;
 
 if (day == 0)
 {   targetName = 'Prologue';   }
 else if (day == 1)
-{   targetName = 'main actor'; tagetVolume = 0.1;   }
+{   targetName = 'main actor';   }
 else if (day == 2)
 {   targetName = 'Monologue';   }
 else if (day == 3)
@@ -14,8 +14,9 @@ else if (day == 3)
 else
 {   targetName = 'おもちゃのダンス';   }
 
+console.log(`Music of the day: ${targetName}\nVolume: ${targetVolume * 100}`);
 let bgm = new Audio(`/media/bgm/${targetName}.mp3`);
-    bgm.volume = 0.2;
+    bgm.volume = targetVolume;
     bgm.loop = true;
 
 let Logo = $('#Logo');
@@ -35,6 +36,7 @@ function PlayBGM() {
         bgm.play();
         $('.tooltip-inner').addClass('c');
     }, 300);
+    console.log(`Playing: ${targetName}`);
 };
 
 function LogoTooltip() {
